cdd489bff98c411ff4a97d896296d3e1c4f8e3a7
Leibruk <mateusz.kurbiel@gmail.com>
2022-05-21 12:36:43 +0200

    Moduły - pakiet

--- Technologie/ModulB.py
+++ Technologie/ModulB.py
@@ -1,5 +1,12 @@
 from DEF_FIND import find as szukaj
 from ModulC import *
+import random
+import sys
 
-print(lista)
-print(szukaj(lista, "Kasia"))
+#(random.__file__)
+#print(lista)
+#print(szukaj(lista, "Kasia"))
+print(sys.path)
+
+for path in sys.path:
+    print(path)
