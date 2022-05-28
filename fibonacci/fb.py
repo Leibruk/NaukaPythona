5fbf0ceba52f944866a767f3491e92adf07a2db0
Leibruk <mateusz.kurbiel@gmail.com>
2022-05-28 13:37:08 +0200

    Ciąg fibonacciego - dokładny test z uzględnieniem liczb ujemnych

--- fibonacci/fb.py
+++ fibonacci/fb.py
@@ -5,3 +5,14 @@ def get_nth(n: int) -> int:
     for i in range(n):
         fib.append(fib[i] + fib[i + 1])
     return fib[n]
+
+if __name__ == '__main__':
+    while True:
+        print("Sprawdź n-ty wyraz ciągu fibonacciego")
+        n = input("Podaj n: ")
+        try:
+            result = get_nth(int(n))
+            print(result)
+        except (ValueError, TypeError) as e:
+            print(e)
+            print("Spróbuj jeszcze raz")
