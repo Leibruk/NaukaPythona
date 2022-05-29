35a5050b3f945ab04781c523e61630630717e2f0
Leibruk <mateusz.kurbiel@gmail.com>
2022-05-29 15:56:30 +0200

    Calculator_Cała reszta

--- Testowanie_TDD/test_calculator.py
+++ Testowanie_TDD/test_calculator.py
@@ -1,11 +1,15 @@
+import io
+
 import pytest
 from electronic_devices.calculator import Calculator, NoBatteryError
 
 
-def test_init_calculator():
+@pytest.fixture()
+def calc():
     calc = Calculator()
     assert calc.memory == 0
     assert calc.battery == 100
+    return calc
 
 
 def test_calculatpor_raise_no_battery_error():
@@ -14,15 +18,45 @@ def test_calculatpor_raise_no_battery_error():
 
 
 @pytest.mark.parametrize("test_values, expected", [((2, 2), 4), ((3, 3, 3), 9)])
-def test_calculator_add_(test_values, expected):
-    calc = Calculator()
+def test_calculator_add_(test_values, expected, calc):
     assert calc.add(*test_values) == expected
     assert calc.memory == expected
     assert calc.battery == 99
 
+
 @pytest.mark.parametrize("test_values, expected", [((2, 2), 0), ((3, 3, 3), -3)])
-def test_calculator_subtract_(test_values, expected):
-    calc = Calculator()
+def test_calculator_subtract_(test_values, expected, calc):
     assert calc.subtract(*test_values) == expected
     assert calc.memory == expected
     assert calc.battery == 99
+
+
+def test_check_calculator_divide_raise_value_error(calc):
+    with pytest.raises(ValueError):
+        calc.divide(5, 0)
+
+
+@pytest.mark.parametrize("test_number, test_power, expected", [(2, 2, 4), (10, 2, 100)])
+def test_check_calculator_to_nth_power(test_number, test_power, expected, calc):
+    assert calc.nth_power(test_number, test_power) == expected
+    assert calc.memory == expected
+    assert calc.battery == 99
+
+
+def test_check_calculator_avg_from_file(mocker, calc):
+    mock__get_data = mocker.patch("electronic_devices.calculator.Calculator._get_data",
+                                  return_value=[[1, 2], [2, 2, 2], [1, 1, 1, 5, 2]])
+    expected = [1.5, 2.0, 2.0]
+    result = calc.avg_from_file()
+    assert result == expected
+    mock__get_data.assert_called_once()
+
+
+def test_check_calculator__get_data(mocker, calc):
+    fake_file = io.StringIO("1,2\n2,2,2\n1,1,1,5,2")
+    mock_os = mocker.patch("electronic_devices.calculator.os")
+    mock_open = mocker.patch("electronic_devices.calculator.open", return_value=fake_file)
+    calc.file = "fake_file.txt"
+    assert calc._get_data() == [[1, 2], [2, 2, 2], [1, 1, 1, 5, 2]]
+    mock_open.asser_called_once_with("fake_file.txt")
+    mock_os.path.isfile.assert_called_once_with("fake_file.txt")
--- electronic_devices/calculator.py
+++ electronic_devices/calculator.py
@@ -1,13 +1,19 @@
+import os
 from typing import Union
 
 class NoBatteryError(ValueError):
     pass
 
 
+class NoFileDefinedError(FileNotFoundError):
+    pass
+
+
 class Calculator:
-    def __init__(self, memory: int = 0, battery: int = 100 ):
+    def __init__(self, memory: int = 0, battery: int = 100, file = None ):
         self.memory = memory
         self.battery = battery
+        self.file = file
         self.check_battery()
 
     def check_battery(self):
@@ -28,6 +34,47 @@ class Calculator:
         result = args[0]
         for n in args[1:]:
             result -= n
+        self.battery -= 1
+        self.memory = result
+        return result
+
+    def divide(self,*args) -> Union[int, float]:
+        self.check_battery()
+        result = args[0]
+        for n in args[1:]:
+            if n == 0:
+                raise ValueError("Pamiętaj cholero nie dziel przez zero!")
+            result /= n
+        self.battery -= 1
+        self.memory = result
+        return result
+
+    def nth_power(self, number: Union[int, float], power: Union[int, float]):
+        self.check_battery()
+        result = number ** power
+        self.battery -= 1
+        self.memory = result
+        return result
+
+    def _get_data(self):
+        if not self.file:
+            raise NoFileDefinedError("Nie zdefiniowałeś pliku!")
+        if os.path.isfile(self.file):
+            with open(self.file) as f:
+                lines = f.readlines()
+            lines_splited = [line.split(",") for line in lines]
+            data = []
+            for line in lines_splited:
+                data.append([int(x) for x in line])
+            return data
+        return None
+
+    def avg_from_file(self):
+        self.check_battery()
+        # https://realpython.com/python-walrus-operator/
+        if not (data := self._get_data()):
+            raise FileNotFoundError("Podana ściężka nie prowadzi do pliku!")
+        result = [sum(x) / len(x) for x in data]
         self.battery -= 1
         self.memory = result
         return result
